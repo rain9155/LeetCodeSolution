da6809bd0cf75ee8cc81e91afb5ec6619cec54c2
rain9155 <jianyu9155@gmail.com>
2019-04-20 20:43:06 +0800

    leetcode题解8

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,10 @@
-import leetcode6.Solution;
+import leetcode8.Solution;
 
 public class Main {
 
     public static void main(String[] args) {
         Solution solution = new Solution();
-        System.out.print(solution.convert("LEE", 4));
+        System.out.print(solution.myAtoi("-"));
     }
 
 
---
+++ src/leetcode8/Solution.java
@@ -0,0 +1,30 @@
+package leetcode8;
+
+/**
+ * 将字符串转换成整数。
+ */
+public class Solution {
+    public int myAtoi(String str) {
+        if(str == null) return 0;
+        str = str.trim();
+        if(str.isEmpty()) return 0;
+        int ret = 0, i;
+        for(i = 0; i < str.length(); ++i){
+            char ch = str.charAt(i);
+            if(i == 0 && (ch == '-' || ch == '+'))
+               continue;
+            if(ch < '0' || ch > '9')
+                break;
+        }
+        str = str.substring(0, i);
+        try {
+            if(str.length() == 0) return 0;
+            char ch = str.charAt(0);
+            if(str.length() == 1 && ((ch == '-' || ch == '+'))) return 0;
+            ret = Integer.valueOf(str);
+        }catch (Exception e){
+            return str.charAt(0) == '-' ? Integer.MIN_VALUE : Integer.MAX_VALUE;
+        }
+        return ret;
+    }
+}
