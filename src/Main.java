8858a4410986c5edff7345ea0c54ea6b5f6eae51
rain9155 <jianyu9155@gmail.com>
2019-06-05 14:42:02 +0800

    添加leetcode69

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,9 @@
-import common.Utils;
-import common.struction.ListNode;
-import medium.leetcode64.Solution;
-import common.List;
+import easy.leetcode69.Solution;
 
 public class Main {
 
     public static void main(String[] args) {
         Solution solution = new Solution();
-        int[][] nums = {{1, 3, 1}, {1, 5, 1}, {4, 2, 1}};
-        System.out.println(solution.minPathSum(nums));
+        System.out.println(solution.mySqrt2(8));
     }
 }
