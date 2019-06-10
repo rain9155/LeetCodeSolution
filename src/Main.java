7841b2c8c2a543c08295a22c0c1703dcbf5f8150
rain9155 <jianyu9155@gmail.com>
2019-06-10 17:04:21 +0800

    upodata

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,18 @@
 import common.Utils;
 import medium.leetcode75.Solution;
 
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.Socket;
+
 public class Main {
 
     public static void main(String[] args) {
         Solution solution = new Solution();
         int[] nums = {2, 0, 2, 1, 1, 0};
-        solution.sortColors(nums);
+        solution.sortColors2(nums);
         Utils.printNums(nums);
     }
 }
--- src/medium/leetcode75/Solution.java
+++ src/medium/leetcode75/Solution.java
@@ -18,9 +18,44 @@ import common.Utils;
  */
 public class Solution {
 
+    /**
+     * 快速排序：
+     * 二路划分
+     */
     public void sortColors(int[] nums) {
         if(nums == null || nums.length == 0) return;
-        Utils.insertionSort(nums);
+        Utils.quickSort(nums);
     }
+
+    /**
+     * 快速排序：
+     * 参考三路划分思想：
+     * 先从序列中选取一个数作为基数(key)；
+     * 分区过程，将<key放到左边，>key的放在右边，=key放到中间；
+     * 再对左右区间重复第二步，直到各区间只有一个数；
+     * 返回的p数组中p[0]代表的是等于区域的左边界，p[1]代表的是等于区域的右边界
+     * 荷兰三色旗问题解：
+     * 用p0指针追踪0的最右边界，用p2指针追踪2的最左边界，用curr指针别追踪当前考虑的元素
+     * 沿着数组移动 curr 指针，若nums[curr] = 0，则将其与 nums[p0]互换， cur++, p0++；
+     * 若 nums[curr] = 2 ，则与 nums[p2]互换, p2--;
+     * 若 nums[curr] = 1 ，则curr++
+     * 当curr 等于 p2时停止
+     */
+    public void sortColors2(int[] nums) {
+        if(nums == null || nums.length == 0) return;
+        int p0 = 0;
+        int curr = 0;
+        int p2 = nums.length - 1;
+        while (curr < p2){
+            if(nums[curr] == 0){
+                Utils.swap(nums, curr++, p0++);
+            }else if(nums[curr] == 2){
+                Utils.swap(nums, curr, p2--);
+            }else {
+                curr++;
+            }
+        }
+    }
+
 
 }
