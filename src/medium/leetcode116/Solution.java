bcebf55f8b0810c811fabb4cee6a1d883c656581
rain9155 <jianyu9155@gmail.com>
2019-07-09 14:54:58 +0800

    updata

---
+++ src/medium/leetcode116/Solution.java
@@ -0,0 +1,72 @@
+package medium.leetcode116;
+
+import common.struction.Node;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * 填充每个节点的下一个右侧节点指针:
+ * 给定一个完美二叉树，其所有叶子节点都在同一层，每个父节点都有两个子节点。二叉树定义如下：
+ * struct Node {
+ *   int val;
+ *   Node *left;
+ *   Node *right;
+ *   Node *next;
+ * }
+ * 填充它的每个 next 指针，让这个指针指向其下一个右侧节点。如果找不到下一个右侧节点，则将 next 指针设置为 NULL。
+ * 初始状态下，所有 next 指针都被设置为 NULL。
+ * 提示：
+ * 你只能使用常量级额外空间。
+ * 使用递归解题也符合要求，本题中递归程序占用的栈空间不算做额外的空间复杂度。
+ */
+public class Solution {
+
+    /**
+     * 使用了额外的空间
+     * 非递归 + 使用队列：
+     * 使用队列保存逐层的结点，然后按层把结点连起来就行
+     */
+    public Node connect(Node root) {
+        if(root == null) return null;
+        Queue<Node> queue = new LinkedList<>();
+        queue.add(root);
+        while (!queue.isEmpty()){
+            int size = queue.size();
+            for(int i = 0; i < size; i++){
+                Node node = queue.poll();
+                if(i != size - 1) node.next = queue.peek();
+                if(node.left != null) queue.add(node.left);
+                if(node.right != null) queue.add(node.right);
+            }
+
+        }
+        return root;
+    }
+
+
+    /**
+     * 不使用额外的空间
+     * 递归：
+     * 按层遍历，每次从最左边的结点开始，记为curNode
+     * 先把curNode的左右结点连起来，如果curNode有next结点，就把curNode的右结点和curNode的next结点的左结点连起来;
+     */
+    public Node connect2(Node root) {
+        if(root == null) return null;
+        Node curNode = root;
+        while (curNode != null){
+            if(curNode.left != null){
+                curNode.left.next = curNode.right;
+            }
+            if(curNode.right != null && curNode.next != null){
+                curNode.right.next = curNode.next.left;
+            }
+            curNode = curNode.next;
+        }
+        connect2(root.left);
+        return root;
+    }
+
+
+
+}
---
+++ src/medium/leetcode117/Solution.java
@@ -0,0 +1,52 @@
+package medium.leetcode117;
+
+import common.struction.Node;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * 填充每个节点的下一个右侧节点指针 II:
+ * 给定一个二叉树
+ * struct Node {
+ *   int val;
+ *   Node *left;
+ *   Node *right;
+ *   Node *next;
+ * }
+ * 填充它的每个 next 指针，让这个指针指向其下一个右侧节点。如果找不到下一个右侧节点，则将 next 指针设置为 NULL。
+ * 初始状态下，所有 next 指针都被设置为 NULL。
+ * 提示：
+ * 你只能使用常量级额外空间。
+ * 使用递归解题也符合要求，本题中递归程序占用的栈空间不算做额外的空间复杂度。
+ */
+public class Solution {
+
+    /**
+     * 递归：
+     * 在递归中用一个list保存当前层的结点，如果当前层结点还没有放入list中，就放入，如果当前层list中已经有了该层的结点，就取出把该结点的next指向目前结点，然后更新该结点
+     */
+    public Node connect(Node root) {
+        if(root == null) return null;
+        connect(root, 0, new ArrayList<>());
+        return root;
+    }
+
+    private void  connect(Node root, int lever, List<Node> list) {
+        if(root == null) return;
+        if(list.size() == lever){
+            //保存当层的结点
+            list.add(root);
+        }else {
+            Node preNode = list.get(lever);
+            preNode.next = root;
+            //更新当前层结点
+            list.set(lever, root);
+        }
+        connect(root.left, lever + 1, list);
+        connect(root.right, lever + 1, list);
+    }
+
+}
