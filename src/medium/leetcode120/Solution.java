92be7f84096edc186a956d56b85073d3b63c78d2
rain9155 <jianyu9155@gmail.com>
2019-07-11 18:33:16 +0800

    updata

--- src/medium/leetcode120/Solution.java
+++ src/medium/leetcode120/Solution.java
@@ -18,8 +18,52 @@ import java.util.List;
  */
 public class Solution {
 
+    /**
+     * 二维dp（动态规划）：
+     * 从triangle的底部往上求，创建一个二维数组dp，一开始把dp的最后一行填充为triangle的最后一行的数字
+     * 然后从triangle的倒数第二行开始求，每一列的数字都是依赖于triangle当前行的当前列的数字和dp的上一行的相邻两个数字的最小数字，即：dp[i][j] = list.get(j) + Math.min(dp[i + 1][j], dp[i + 1][j + 1]);
+     * 最后dp[0][0]就是结果
+     */
     public int minimumTotal(List<List<Integer>> triangle) {
-
+        if(triangle == null || triangle.size() == 0 || triangle.get(0).size() == 0) return 0;
+        int n = triangle.size();
+        int m = triangle.get(triangle.size() - 1).size();
+        int[][] dp = new int[n][m];
+        //先填充最后一行
+        for(int j = 0; j < triangle.get(triangle.size() - 1).size(); j++){
+            dp[triangle.size() - 1][j] = triangle.get(triangle.size() - 1).get(j);
+        }
+        for(int i = n - 2; i >= 0; i--){
+            List<Integer> list = triangle.get(i);
+            for(int j = 0; j < list.size(); j++){
+                dp[i][j] = list.get(j) + Math.min(dp[i + 1][j], dp[i + 1][j + 1]);
+            }
+        }
+        return dp[0][0];
     }
+
+    /**
+     * 一维dp(动态规划):
+     * 因为二维dp中每一个元素都依赖于dp[i + 1][j], dp[i + 1][j + 1]，并只会把更新后的值放在dp[i][j], 而dp[i + 1][j + 1]的值会继续使用
+     * 所以我们可以把[j]维去掉，首先把dp填充为triangle的最后一行的数字，然后然后从triangle的倒数第二行开始求
+     * 每相邻两个数字的最小数字和triangle当前行的当前列的数字的和放在dp的当前位置j
+     * 最后dp[0]就是结果
+     */
+    public int minimumTotal2(List<List<Integer>> triangle) {
+        if(triangle == null || triangle.size() == 0 || triangle.get(0).size() == 0) return 0;
+        int n = triangle.size();
+        int[] dp = new int[n];
+        for(int j = 0; j < triangle.get(triangle.size() - 1).size(); j++){
+            dp[j] = triangle.get(triangle.size() - 1).get(j);
+        }
+        for(int i = n - 2; i >= 0; i--){
+            List<Integer> list = triangle.get(i);
+            for(int j = 0; j < list.size(); j++){
+                dp[j] = list.get(j) + Math.min(dp[j], dp[j + 1]);
+            }
+        }
+        return dp[0];
+    }
+
 
 }
