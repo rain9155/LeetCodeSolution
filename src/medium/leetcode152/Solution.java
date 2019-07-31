5655e5f8d611c1b8e7018148f352f7e77d4c014e
rain9155 <jianyu9155@gmail.com>
2019-07-31 12:54:13 +0800

    leetcode153

--- src/medium/leetcode152/Solution.java
+++ src/medium/leetcode152/Solution.java
@@ -31,13 +31,31 @@ public class Solution {
         return max;
     }
 
+    /**
+     * O(n)
+     * 动态规划：
+     * 遍历数组时计算当前最大值max，不断更新
+     * 令tempMax为当前最大值，则当前最大值为：  tempMax = Math.max(nums[i], tempMax * nums[i])
+     * 由于存在负数，那么会导致最大的变最小的，最小的变最大的，因此还需要维护前最小值：tempMin = Math.min(nums[i], tempMin * nums[i]);
+     * 当负数出现时则tempMax与tempMin进行交换再进行下一步计算
+     */
     public int maxProduct2(int[] nums) {
         if(nums == null || nums.length == 0) return 0;
         int max = nums[0];
-        int tempMax = 1;
+        int tempMax = max;
+        int tempMin = max;
         for(int i = 0; i < nums.length; i++){
+
+            if(nums[i] < 0){
+                int temp = tempMax;
+                tempMax = tempMin;
+                tempMin = temp;
+            }
+
             tempMax = Math.max(nums[i], tempMax * nums[i]);
-            max = Math.max(tempMax, max);//比较当前数和最大数哪一个数最大 和 比较连续数和最大数哪一个数最大
+            tempMin = Math.min(nums[i], tempMin * nums[i]);
+
+            max = Math.max(tempMax, max);
         }
         return max;
     }
