3e6de7f97bdfcdcb761ee1b612ae18d33c31e31a
rain9155 <jianyu9155@gmail.com>
2019-10-16 19:13:39 +0800

    leetcode355

---
+++ src/easy/leetcode349/Solution.java
@@ -0,0 +1,52 @@
+package easy.leetcode349;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * 两个数组的交集:
+ * 给定两个数组，编写一个函数来计算它们的交集。
+ *
+ * 示例 1:
+ * 输入: nums1 = [1,2,2,1], nums2 = [2,2]
+ * 输出: [2]
+ * 示例 2:
+ * 输入: nums1 = [4,9,5], nums2 = [9,4,9,8,4]
+ * 输出: [9,4]
+ *
+ * 说明:
+ * 输出结果中的每个元素一定是唯一的。
+ * 我们可以不考虑输出结果的顺序。
+ */
+public class Solution {
+
+    /**
+     * 哈希表：
+     */
+    public int[] intersection(int[] nums1, int[] nums2) {
+        if(nums1.length == 0 || nums2.length == 0) return new int[0];
+        if(nums2.length > nums1.length){
+            int[] temp = nums1;
+            nums1  = nums2;
+            nums2 = temp;
+        }
+        Set<Integer> set1 = new HashSet<>();
+        for(int num : nums1){
+            set1.add(num);
+        }
+        List<Integer> list = new ArrayList<>();
+        for(int num : nums2){
+            if(set1.contains(num)){
+                list.add(num);
+            }
+        }
+        int[] ret = new int[list.size()];
+        for(int i= 0; i < list.size(); i++){
+            ret[i] = list.get(i);
+        }
+        return ret;
+    }
+
+}
---
+++ src/medium/leetcode355/Twitter.java
@@ -0,0 +1,152 @@
+package medium.leetcode355;
+
+import java.util.*;
+
+/**
+ * 设计推特:
+ * 设计一个简化版的推特(Twitter)，可以让用户实现发送推文，关注/取消关注其他用户，能够看见关注人（包括自己）的最近十条推文。你的设计需要支持以下的几个功能：
+ * postTweet(userId, tweetId): 创建一条新的推文
+ * getNewsFeed(userId): 检索最近的十条推文。每个推文都必须是由此用户关注的人或者是用户自己发出的。推文必须按照时间顺序由最近的开始排序。
+ * follow(followerId, followeeId): 关注一个用户
+ * unfollow(followerId, followeeId): 取消关注一个用户
+ *
+ * 示例:
+ * Twitter twitter = new Twitter();
+ *
+ * // 用户1发送了一条新推文 (用户id = 1, 推文id = 5).
+ * twitter.postTweet(1, 5);
+ *
+ * // 用户1的获取推文应当返回一个列表，其中包含一个id为5的推文.
+ * twitter.getNewsFeed(1);
+ *
+ * // 用户1关注了用户2.
+ * twitter.follow(1, 2);
+ *
+ * // 用户2发送了一个新推文 (推文id = 6).
+ * twitter.postTweet(2, 6);
+ *
+ * // 用户1的获取推文应当返回一个列表，其中包含两个推文，id分别为 -> [6, 5].
+ * // 推文id6应当在推文id5之前，因为它是在5之后发送的.
+ * twitter.getNewsFeed(1);
+ *
+ * // 用户1取消关注了用户2.
+ * twitter.unfollow(1, 2);
+ *
+ * // 用户1的获取推文应当返回一个列表，其中包含一个id为5的推文.
+ * // 因为用户1已经不再关注用户2.
+ * twitter.getNewsFeed(1);
+  *
+ * Your Twitter object will be instantiated and called as such:
+ * Twitter obj = new Twitter();
+ * obj.postTweet(userId,tweetId);
+ * List<Integer> param_2 = obj.getNewsFeed(userId);
+ * obj.follow(followerId,followeeId);
+ * obj.unfollow(followerId,followeeId);
+ */
+
+/**
+ * 哈希表 + 大顶堆：
+ * 1、用一个哈希表维护每个用户发过的推特，当postTweet时，更新这个哈希表
+ * 2、再用一个哈希表维护每个用户关注的人，当follow或unFollow时，更新这个哈希表
+ * 3、每一个推特都有一个时间戳，发布时间越近，值越大，当getNewsFeed时，把用户发过的推特和关注人发过的推特放入一个大顶堆中，最后取大顶堆前10篇推特即返回结果
+ */
+public class Twitter {
+
+    private static final int COUNT = 10;
+    private HashMap<Integer, Set<Integer>> followers = new HashMap<>();
+    private HashMap<Integer, List<Tweet>> tweets = new HashMap<>();
+    private Queue<Tweet> newsFeed = new PriorityQueue<>(COUNT, new Comparator<Tweet>() {
+        @Override
+        public int compare(Tweet o1, Tweet o2) {
+            return Integer.compare(o2.postTime, o1.postTime);
+        }
+    });
+    private int postTime = 0;//时间戳
+
+    /** Initialize your data structure here. */
+    public Twitter() {}
+
+    /**
+     * userId 发表id为 tweetId 的Tweet
+     */
+    public void postTweet(int userId, int tweetId) {
+        tweets.computeIfAbsent(userId, k -> new LinkedList<>());
+        List<Tweet> postTweets = tweets.get(userId);
+        Tweet tweet = new Tweet(tweetId, ++postTime);
+        postTweets.add(tweet);
+    }
+
+    /**
+     * userId 获取关注人和自己的最近十条Tweet的tweetId
+     */
+    public List<Integer> getNewsFeed(int userId) {
+        newsFeed.clear();
+        //获取自己发布的文章
+        if(tweets.containsKey(userId)){
+            List<Tweet> postTweets = tweets.get(userId);
+            addToNewsFeed(postTweets);
+        }
+        //获取关注人发布的文章
+        if(followers.containsKey(userId)){
+            Set<Integer> followees = followers.get(userId);
+            for(Integer followeeId : followees){
+                List<Tweet> followeePostTweets = tweets.get(followeeId);
+                if(followeePostTweets != null){
+                    addToNewsFeed(followeePostTweets);
+                }
+            }
+        }
+        //返回结果
+        LinkedList<Integer> ret = new LinkedList<>();
+        while (!newsFeed.isEmpty() && ret.size() < 10){
+            Tweet tweet = newsFeed.poll();
+            ret.add(tweet.id);
+        }
+        return ret;
+    }
+
+    /**
+     * followerId 关注 followeeId
+     */
+    public void follow(int followerId, int followeeId) {
+        if(followerId == followeeId) return;
+        followers.computeIfAbsent(followerId, k -> new HashSet<>());
+        Set<Integer> followees = followers.get(followerId);
+        followees.add(followeeId);
+    }
+
+    /**
+     * followerId 取消关注 followeeId
+     */
+    public void unfollow(int followerId, int followeeId) {
+        if(followers.containsKey(followerId)){
+            Set<Integer> followees = followers.get(followerId);
+            followees.remove(followeeId);
+            if(followees.isEmpty()){
+                followers.remove(followerId);
+            }
+        }
+    }
+
+    /**
+     * 维护一个大顶堆，越靠近堆顶，Tweet发布时间最近
+     */
+    private void addToNewsFeed(List<Tweet> tweets){
+        for(Tweet tweet : tweets){
+            newsFeed.add(tweet);
+        }
+    }
+
+
+    class Tweet{
+
+        int id;
+        int postTime;
+
+        Tweet(int id, int time){
+            this.id = id;
+            this.postTime = time;
+        }
+    }
+
+}
