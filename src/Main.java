cb83d2cc610dd384abf4185c735ed537da906404
rain9155 <jianyu9155@gmail.com>
2019-10-21 22:10:50 +0800

    leetcode373

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,11 @@
 import common.Utils;
-import medium.leetcode368.Solution;
+import medium.leetcode373.Solution;
 
 
 public class Main {
 
     public static void main(String[] args) {
         Solution solution = new Solution();
-        solution.largestDivisibleSubset(new int[]{1, 2, 3});
+        solution.kSmallestPairs(new int[]{1, 7, 11}, new int[]{2, 4, 6}, 3);
     }
 }
