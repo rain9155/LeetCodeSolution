3a3dc4eceda9d7264596039b1ae8ff5439432ac5
chenjianyu <chenjianyu.rain@bytedance.com>
2023-03-20 19:16:22 +0800

    update

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,7 @@
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
 
 public class Main {
 
     public static void main(String[] args) {
-
+        System.out.println("hello world!");
     }
-
-
-
-
-
 }
