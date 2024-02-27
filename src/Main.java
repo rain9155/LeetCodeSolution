b9242f49fde6ada4d1208566509a12d7021233f0
chenjianyu <chenjianyu.rain@bytedance.com>
2024-02-27 20:17:21 +0800

    vs添加cpp文件夹

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,10 @@
+import easy.leetcode7.*;;
 
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("hello world!");
+        Solution solution = new Solution();
+        int result = solution.reverse(1234);
+        System.err.println("result = " + result);
     }
 }
